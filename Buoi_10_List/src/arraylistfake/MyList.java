0f32f62ec04c71f3dcd4eaa333947fb6531a7a66
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-09-23 14:32:50 +0700

    arraylist

---
+++ Buoi_10_List/src/arraylistfake/MyList.java
@@ -0,0 +1,82 @@
+package arraylistfake;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    public Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public void add(int index, E element) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            if (i == index) {
+                for (int j = elements.length - 1; j > index; j--) {
+                    elements[j] = elements[j - 1];
+                }
+                elements[i] = element;
+            }
+        }
+    }
+
+    public E remove(int index) {
+        for (int i = 0; i < elements.length - 1; i++) {
+            if (i == index) {
+                for (int j = index; j < elements.length - 1; j++) {
+                    elements[j] = elements[j + 1];
+                }
+            }
+        }
+        return (E) elements[index];
+    }
+
+    public E get(int i) {
+        if (i >= elements.length || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        return (E) elements[i];
+    }
+
+    public E clone1() {
+        Object[] arrNew = new Object[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            arrNew[i] = elements[i];
+        }
+        return (E) arrNew;
+    }
+
+    public boolean contains(E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(E o) {
+        int index = 0;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == o) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
---
+++ Buoi_10_List/src/arraylistfake/MyListTest.java
@@ -0,0 +1,22 @@
+package arraylistfake;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(0, 1);
+        listInteger.add(1, 2);
+        listInteger.add(2, 3);
+        listInteger.add(3, 4);
+        listInteger.add(3, 1000);
+        listInteger.remove(1);
+
+//        MyList<Integer> listNew = (MyList<Integer>) listInteger.clone1();
+        System.out.println("Phần tử 3 có trong mảng không? " + listInteger.contains(3)); // true
+        System.out.println("Vị trí của phần tử số 3: " + listInteger.indexOf(3)); // 1
+        System.out.println("Phần tử có vị trí 1 là: " + listInteger.get(1)); // 3
+
+        for (Object x : listInteger.elements) {
+            System.out.println(x);
+        }
+    }
+}
