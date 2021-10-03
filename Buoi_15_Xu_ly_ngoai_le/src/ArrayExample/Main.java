aa123c6a6e72d6cc2539a968e97ddd7b55d1d4d9
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-10-03 23:26:52 +0700

    a

---
+++ Buoi_15_Xu_ly_ngoai_le/src/ArrayExample/Main.java
@@ -0,0 +1,23 @@
+package ArrayExample;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+
+        try {
+            int x = scanner.nextInt();
+            System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
+        } catch (InputMismatchException e) {
+            System.out.println("Nhập sai đầu vào");
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
