ae28421666fc87d9861cb8fe24a464aff2cd656a
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-10-11 20:53:03 +0700

    racing car

---
+++ Buoi_18_Thread/src/racingcar/Car.java
@@ -0,0 +1,52 @@
+package racingcar;
+
+import java.util.Random;
+
+import static racingcar.Main.DISTANCE;
+import static racingcar.Main.STEP;
+
+public class Car implements Runnable {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        // Khởi tạo điểm start(hay km ban đầu)
+        int runDistance = 0;
+        // Khởi tạo time bắt đầu cuộc đua
+        long startTime = System.currentTimeMillis();
+
+        // Kiểm tra chừng nào còn xe chưa kết thúc quãng đường đua thì xe vẫn tiếp tục chạy
+        while (runDistance < DISTANCE) {
+            try {
+                // Random Speed KM/H
+                int speed = (new Random()).nextInt(20);
+                // Tính quãng đường đã đi
+                runDistance += speed;
+                // Xây dựng đồ họa kết quả
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i < DISTANCE; i += STEP) {
+                    if (percentTravel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+        }
+    }
+}
